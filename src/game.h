7c2c2fade47e2b36bf2dec2e54b7f742e6324289
Mathis Sinet <mathis.sinet@gmail.com>
2024-04-17 18:31:32 +0200

    Added structures for Item, Player, Room, Region

--- src/game.h
+++ src/game.h
@@ -1,11 +1,13 @@
 #include "settings.h"
 
+//Structure for the items
 typedef struct Item
 {
     
 }
 Item;
 
+//Structure for the informations of the player
 typedef struct Player
 {
     char name[MAX_PLAYER_NAME_COUNT];
@@ -15,3 +17,29 @@ typedef struct Player
     uint32_t def;
 }
 Player;
+
+typedef struct Room
+{
+    bool is_generated; //is the room generated
+
+    //coordinates of the top-left corner of the room
+    uint16_t x; 
+    uint16_t y;
+
+    uint8_t width;
+    uint8_t height;
+
+    uint8_t door_count;
+}
+Room;
+
+typedef struct Region
+{
+    uint64_t seed;
+    int8_t **grid;
+    Room *roomlist;
+    uint16_t allocated_rooms;
+}
+Region;
+
+void allocate_room(Region*, uint16_t, uint16_t);
